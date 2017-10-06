18c0cad6322bd620732f125254ca67b4024a116c
oussama4 <oussamazouaki4@gmail.com>
2017-10-06 22:27:11 +0100

    week 1 assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,20 @@ import { MatToolbarModule } from '@angular/material';
 import { MatGridListModule } from '@angular/material';
 import { MatCardModule } from '@angular/material';
 import { MatButtonModule } from '@angular/material';
+import { MatListModule } from '@angular/material';
 
 
 import 'hammerjs';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './menu/dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +29,7 @@ import { MenuComponent } from './menu/menu.component';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
